4b455518d1ca358fb6c4773a5b6c663783e9466f
David Gamet <atelierdufilm@orange.fr>
2018-01-19 15:42:23 +0100

    controllerstaticpages

---
+++ app/controllers/staticpages_controller.rb
@@ -0,0 +1,19 @@
+class StaticpagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def clips
+  end
+
+  def présentation
+  end
+
+  def anthropologie
+  end
+
+  def visuelle
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'staticpages/home'
+
+  get 'staticpages/contact'
+
+  get 'staticpages/clips'
+
+  get 'staticpages/présentation'
+
+  get 'staticpages/anthropologie'
+
+  get 'staticpages/visuelle'
+
   root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
